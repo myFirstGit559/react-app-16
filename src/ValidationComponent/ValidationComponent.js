1e1670cc8f21592a87192754f59a282c9097d711
Tanya Agisheva <tanyaagisheva@MacBook-Pro-Tanya.local>
2018-07-01 20:13:27 +0300

    small improvement

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -4,9 +4,11 @@ import './ValidationComponent.css'
 const validationComponent = (props) => {
   return (
     <div>
-    {(props.length!==0)?(
+    {(props.length!==0) && (
       (props.length < 5)?
-    <p className="error">Text too short</p>:<p className="valid">Text long enough</p>):null}
+        <p className="error">Text too short</p>:
+        <p className="valid">Text long enough</p>
+    )}
     </div>
   )}
 
