fb0fc21f4132fe71155430bd9475295ee8043176
Tanya Agisheva <tanyaagisheva@MacBook-Pro-Tanya.local>
2018-07-08 19:27:51 +0300

    mobile menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,13 +4,16 @@ import classes from './Toolbar.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggler/DrawerToggle';
 
 const toolbar = (props) => {
  return (
    <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <nav>
+    <DrawerToggle clicked={props.DrawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesctopOnly}>
       <NavigationItems />
     </nav>
    </header>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -13,7 +13,8 @@ const modal = (props) => {
      <div className={classes.Modal}
      style={{
        translate: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-       opacity: props.show ? '1' : '0'
+       opacity: props.show ? '1' : '0',
+       display: props.show ? 'block' : 'none'
      }}>
       {props.children}
      </div>
